2119a7cec61488f81d6270b3d38d4ca06513362d
Mary Lueder <mlueder@gmail.com>
2022-12-07 17:36:22 -0500

    solve core lab challenge

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+const inputEl = document.getElementById("input-text")
+const btnEl = document.getElementById("submit-button")
+const ulEl = document.getElementById("todo-list")
+
+// console.log(inputEl);
+// console.log(btnEl);
+// console.log(ulEl);
+
+btnEl.addEventListener('click', function(evt){
+  // console.log(evt);
+  const li = document.createElement('li')
+  // console.log(li);
+  li.textContent = inputEl.value 
+  // console.log(li);
+  ulEl.appendChild(li)
+  inputEl.value = ''
+})
