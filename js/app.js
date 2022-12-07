f5efa3070ca9598dfc2650d38c0592cb6059df83
Mary Lueder <mlueder@gmail.com>
2022-12-07 18:14:09 -0500

    attempt reset button

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@
 const inputEl = document.getElementById("input-text")
 const btnEl = document.getElementById("submit-button")
 const ulEl = document.getElementById("todo-list")
+const resetEl = document.getElementById("reset-button")
 
 // console.log(inputEl);
 // console.log(btnEl);
 // console.log(ulEl);
+// console.log(resetEl);
 
 btnEl.addEventListener('click', function(evt){
   // console.log(evt);
@@ -16,4 +18,11 @@ btnEl.addEventListener('click', function(evt){
   // console.log(li);
   ulEl.appendChild(li)
   inputEl.value = ''
+})
+
+resetEl.addEventListener('click', function(evt){
+  const listItems = document.querySelectorAll('li')
+  console.log(listItems);
+  listItems.remove(listItems)
+  // ulEl.removeChild(listItems)
 })
